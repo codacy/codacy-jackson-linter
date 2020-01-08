fb6626bd04e541abb719dea8a7fe63d3ac2bcfcf
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-01-08 20:09:40 +0100

    clean: Update Scala to 2.13
    - Use Scalafmt
    - Rename tool to codacy-jackson-linter

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,2 @@
 resolvers += Resolver.jcenterRepo
-libraryDependencies ++= Seq("com.github.pathikrit" %% "better-files" % "3.6.0")
-
 addSbtPlugin("com.codacy" % "codacy-sbt-plugin" % "17.1.5")
--- src/main/scala/codacy/jsonLint/JsonLint.scala
+++ src/main/scala/codacy/jsonLint/JsonLint.scala
@@ -9,65 +9,88 @@ import com.fasterxml.jackson.core.{JsonParseException, JsonParser}
 import com.fasterxml.jackson.databind.ObjectMapper
 
 import scala.util.{Failure, Try}
-
+import com.fasterxml.jackson.core.json.JsonReadFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
 
 private object JsonPattern extends Enumeration {
-  lazy val allPatterns: Set[Pattern.Id] = JsonPattern.values.map(JsonPattern.toPatternId)
+  lazy val allPatterns: Set[Pattern.Id] =
+    JsonPattern.values.unsorted.map(JsonPattern.toPatternId)
   val duplicateField = Value("duplicate-keys")
   val invalidJson = Value("parse-error")
 
-  def fromPatternId(Id: Pattern.Id): Option[JsonPattern.Value] = Try {
-    this.withName(Id.value)
-  }.toOption
+  def fromPatternId(Id: Pattern.Id): Option[JsonPattern.Value] =
+    Try {
+      this.withName(Id.value)
+    }.toOption
 
-  def toPatternId(pattern: JsonPattern.Value): Pattern.Id = Pattern.Id(pattern.toString)
+  def toPatternId(pattern: JsonPattern.Value): Pattern.Id =
+    Pattern.Id(pattern.toString)
 }
 
 object JsonLint extends Tool {
 
   private lazy val parser: ObjectMapper = {
-    val objectMapper = new ObjectMapper()
-    objectMapper.enable(JsonParser.Feature.ALLOW_COMMENTS)
-    objectMapper.enable(JsonParser.Feature.ALLOW_YAML_COMMENTS)
-    objectMapper.enable(JsonParser.Feature.ALLOW_SINGLE_QUOTES)
-    objectMapper.enable(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS)
-    objectMapper.enable(JsonParser.Feature.ALLOW_NUMERIC_LEADING_ZEROS)
-    objectMapper.enable(JsonParser.Feature.ALLOW_NON_NUMERIC_NUMBERS)
-    objectMapper.enable(JsonParser.Feature.IGNORE_UNDEFINED)
-    objectMapper.enable(JsonParser.Feature.STRICT_DUPLICATE_DETECTION)
-
-    objectMapper
+    JsonMapper
+      .builder()
+      .enable(JsonReadFeature.ALLOW_JAVA_COMMENTS)
+      .enable(JsonReadFeature.ALLOW_YAML_COMMENTS)
+      .enable(JsonReadFeature.ALLOW_SINGLE_QUOTES)
+      .enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS)
+      .enable(JsonReadFeature.ALLOW_LEADING_ZEROS_FOR_NUMBERS)
+      .enable(JsonReadFeature.ALLOW_NON_NUMERIC_NUMBERS)
+      .enable(JsonParser.Feature.IGNORE_UNDEFINED)
+      .enable(JsonParser.Feature.STRICT_DUPLICATE_DETECTION)
+      .build()
   }
 
-  def apply(source: Source.Directory,
-            configuration: Option[List[Pattern.Definition]],
-            files: Option[Set[Source.File]],
-            options: Map[Options.Key, Options.Value])(implicit specification: Tool.Specification): Try[List[Result]] = {
+  def apply(
+      source: Source.Directory,
+      configuration: Option[List[Pattern.Definition]],
+      files: Option[Set[Source.File]],
+      options: Map[Options.Key, Options.Value]
+  )(implicit specification: Tool.Specification): Try[List[Result]] = {
 
-    val filesList = files.map(_.map(file => File(file.path))).getOrElse(File(source.path).listRecursively.toSet)
+    val filesList = files
+      .map(_.map(file => File(file.path)))
+      .getOrElse(File(source.path).listRecursively.toSet)
 
-    val issuesList = filesList.collect { case file =>
-      Try {
-        parser.readTree(file.toJava)
-      } match {
-        case Failure(exp: JsonParseException) =>
-          parseException(exp, file, configuration)
-        case Failure(exp) =>
-          Option(Result.FileError(Source.File(file.path.toString), Option(ErrorMessage(exp.getMessage))))
-        case _ =>
-          None
-      }
+    val issuesList = filesList.collect {
+      case file =>
+        Try {
+          parser.readTree(file.toJava)
+        } match {
+          case Failure(exp: JsonParseException) =>
+            parseException(exp, file, configuration)
+          case Failure(exp) =>
+            Option(
+              Result.FileError(
+                Source.File(file.path.toString),
+                Option(ErrorMessage(exp.getMessage))
+              )
+            )
+          case _ =>
+            None
+        }
     }.flatten
 
     Try(issuesList.toList)
   }
 
-  private def parseException(exp: JsonParseException, file: File, configuration: Option[List[Definition]]): Option[Result] = {
+  private def parseException(
+      exp: JsonParseException,
+      file: File,
+      configuration: Option[List[Definition]]
+  ): Option[Result] = {
     def createIssue(pattern: JsonPattern.Value, message: String) = {
-      filterResult(Result.Issue(Source.File(file.path.toString),
-        Result.Message(message),
-        Pattern.Id(pattern.toString),
-        Source.Line(exp.getLocation.getLineNr)), configuration)
+      filterResult(
+        Result.Issue(
+          Source.File(file.path.toString),
+          Result.Message(message),
+          Pattern.Id(pattern.toString),
+          Source.Line(exp.getLocation.getLineNr)
+        ),
+        configuration
+      )
     }
 
     val duplicate = """^(Duplicate field .*)""".r
@@ -77,16 +100,23 @@ object JsonLint extends Tool {
       case duplicate(msg) =>
         createIssue(JsonPattern.duplicateField, msg)
       case msg =>
-        val message = msg.replaceFirst("\\(not recognized as one since Feature 'ALLOW_COMMENTS' not enabled for parser\\)", "").trim
+        val message = msg
+          .replaceFirst(
+            "\\(not recognized as one since Feature 'ALLOW_COMMENTS' not enabled for parser\\)",
+            ""
+          )
+          .trim
         createIssue(JsonPattern.invalidJson, message)
     }
   }
 
+  private def filterResult(
+      issue: Result.Issue,
+      configuration: Option[List[Definition]]
+  ): Option[Issue] = {
+    val enabledPatterns =
+      configuration.fold(JsonPattern.allPatterns)(_.map(_.patternId).toSet)
 
-  private def filterResult(issue: Result.Issue, configuration: Option[List[Definition]]): Option[Issue] = {
-    val enabledPatterns = configuration.fold(JsonPattern.allPatterns)(_.map(_.patternId).to[Set])
-
-    Option(issue).filter(result =>
-      enabledPatterns.contains(result.patternId))
+    Option(issue).filter(result => enabledPatterns.contains(result.patternId))
   }
 }
